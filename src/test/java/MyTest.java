3b1a9edf73a4b82df4612c07ffd8a35691860cd2
nelol <neloliver@hotmail.com>
2023-05-31 15:57:21 +0400

    steps added

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,7 +1,9 @@
-import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.*;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.time.Duration;
-import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,36 +11,36 @@ public class MyTest {
 
     @Test
     void myTest() {
-        //Step 1: Открытие Yandex Mail страницы для входа
         openLoginPage();
-
-        // Step 2:  Ввод учетных данных и выполнение входа на почту
         loginToMail(MailCredentials.USERNAME, MailCredentials.PASSWORD);
-
-        // Step 3: Проверка заголовка страницы
-        assertPageTitle("Авторизация");
-
-        //Step 4: Проверка видимости всех полей ввода на странице
+        assertPageTitle("Вход в учетную запись Майкрософт");
         checkAllInputFieldsVisible();
-
     }
 
+    @Step("Opening the Outlook Mail page to sign in")
     private void openLoginPage() {
-        open("https://passport.yandex.ru/auth?retpath=https%3A%2F%2Fmail.yandex.ru");
+
+        Selenide.open("https://outlook.live.com/owa/?nlp=1");
     }
 
+    @Step("Entry of credentials")
     private void loginToMail(String username, String password) {
-        $(Selectors.byXpath("//input[@id='passp-field-login']")).setValue(MailCredentials.USERNAME).submit();
-        $(Selectors.byXpath("//input[@id='passp-field-passwd']")).setValue(MailCredentials.PASSWORD).submit();
+        $(byXpath("//input[@id='i0116']")).setValue(MailCredentials.USERNAME).pressEnter();
+        $(byXpath("//input[@id='i0118']")).setValue(MailCredentials.PASSWORD).pressEnter();
     }
 
+    @Step("Checking the page title")
     private void assertPageTitle(String expectedTitle) {
         String actualTitle = title();
-        assertEquals("Авторизация", actualTitle);
+        assertEquals(expectedTitle, actualTitle);
     }
 
+    @Step("Checking the visibility of all input fields on the page")
     private void checkAllInputFieldsVisible() {
-        $(Selectors.byXpath("//input")).shouldBe(visible, Duration.ofSeconds(10));
+        Configuration.timeout = 5000;
+        ElementsCollection visibleFields = $$(byXpath("//input[@type='text' and @visible='true'] | //textarea[@visible='true']"));
+        for (SelenideElement field : visibleFields) {
+            Assertions.assertTrue(field.isEnabled());
+        }
     }
 }
-
