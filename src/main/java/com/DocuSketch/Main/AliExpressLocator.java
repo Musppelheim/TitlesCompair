7f818b750a12e6a1e4b71cbe80c346f3e426e946
nelol <neloliver@hotmail.com>
2023-06-19 16:20:23 +0400

    allure folders removed and Xpath edited

--- src/main/java/com/DocuSketch/Main/AliExpressLocator.java
+++ src/main/java/com/DocuSketch/Main/AliExpressLocator.java
@@ -4,18 +4,17 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AliExpressLocator {
-    public static final SelenideElement popUpWindow = $(byXpath("//*[@id=\"__aer_root__\"]/div/div[5]/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/div/div/button[1]"));
+    public static final SelenideElement popUpWindow = $(byXpath("//button[@type='button' and text()='Да, продолжить']"));
 
-    public static final SelenideElement computerMenu = $(byXpath("//*[@id=\"__aer_root__\"]/div/div[8]/div[1]/div/div/div[1]/div/ul/li[4]/a/div"));
+    public static final SelenideElement computerMenu = $(byXpath("//li[contains(@exp_attribute, 'category_id=4')]"));
 
-    public static final SelenideElement subCategory = $(byXpath("//*[@id=\"__aer_root__\"]/div/div[5]/div[1]/div[2]/div/a[2]"));
+    public static final SelenideElement tabletsTab = $(byXpath("//a[contains(@exp_attribute, 'categoryId=16006')]"));
 
-    public static final SelenideElement lenovoCheckbox = $(byXpath("//*[@id=\"__aer_root__\"]/div/div[5]/div[2]/div[1]/div/div[3]/div/div[5]/ul/li[4]/label/span[1]"));
+    public static final SelenideElement lenovoCheckbox = $(byXpath("//li[contains(@exp_attribute, '1609-2361')]//label"));
 
-    public static final ElementsCollection numberOfItems = $$(byXpath("//*[@id=\"__aer_root__\"]/div/div[5]/div[2]/div[2]/div[1]/div[1]/div"));
+    public static final ElementsCollection itemCount = $$x("//*[contains(@class, 'product-snippet_ProductSnippet__grid__17ub13')]");
 
 }
--- src/main/java/com/DocuSketch/Main/AliExpressSteps.java
+++ src/main/java/com/DocuSketch/Main/AliExpressSteps.java
@@ -2,46 +2,57 @@ package com.DocuSketch.Main;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.DocuSketch.Main.AliExpressLocator.*;
 
 public class AliExpressSteps {
-    @Step
+    @Step ("Open AliExpress market")
     public static void openOnlineShop() {
         Selenide.open("https://aliexpress.ru/?gatewayAdapt=glo2rus");
 
     }
 
-    @Step
+    @Step("Close the pop-up window displayed")
     public static void closePopUpWindow() {
         if (popUpWindow.isDisplayed()) {
             popUpWindow.click();
         }
     }
 
-    @Step
+    @Step("Select the 'Computer' menu")
     public static void selectComputers() {
         computerMenu.shouldBe(Condition.visible).click();
 
     }
 
-    @Step
+    @Step("Select the 'Tablets' sub-menu")
     public static void selectTabletsTab() {
-        subCategory.shouldBe(Condition.visible).pressTab();
+        tabletsTab.shouldBe(Condition.visible).click();
 
     }
 
-    @Step
+    @Step("Select checkbox 'Lenovo'")
     public static void checkLenovoCheckbox() {
+        Selenide.executeJavaScript("window.scrollBy(0,400)");  // scroll down
         lenovoCheckbox.shouldBe(Condition.visible).click();
 
     }
 
-    @Step
+    @Step("Display the number of items found")
     public static void printNumberOfItems() {
-        int numberOfItemsDisplayed = numberOfItems.size();
-        System.out.println("Number of items found: " + numberOfItemsDisplayed);
+        List<String> productIds = new ArrayList<>();
+        for (SelenideElement element : itemCount) {
+            String productId = element.getAttribute("data-product-id");
+            if (productId != null)
+            productIds.add(productId);
+        }
+        System.out.println("Total number of items: " + productIds.size());
+        }
     }
-}
+
 
